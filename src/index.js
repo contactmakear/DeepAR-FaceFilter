f3622d245876f231397a216c4031ce48febeda5f
Rachit <mailracit@gmail.com>
2025-01-30 18:51:08 +0530

    api key changed

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ if (document.querySelector('#ar-screen')) {
 
     try {
       deepAR = await deepar.initialize({
-        licenseKey: "26f99e4a4c4526f420c18a101ef06d89a9aa64214c076cb765adc6ac6ea71985f974515335c431c8",
+        licenseKey: "2a51777dc1f79fc3c597fb49c754394bf68f897727fb2cdc5d4177ea9976b5e2c682c0175655fff1",
         previewElement,
         effect: effectList[0],
         rootPath: "./deepar-resources",
