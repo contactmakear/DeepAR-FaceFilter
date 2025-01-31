3ad30c32cccf4d292a12063cc0e983ed54c5f401
rachit <mailracit@gmail.com>
2025-01-31 13:09:12 +0530

    wow

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "public")))
-app.use(express.static("public/effects"))
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
