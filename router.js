ba31abee6d6f9e06c228f8193f9c48f4e69b40c6
prashant <prashant.sharma@makear.ai>
2025-02-11 18:37:57 +0530

    testing

--- router.js
+++ router.js
@@ -5,7 +5,9 @@ const { upload } = require("./src/imageHandler");
 const router = express.Router()
 
 router.get('/', userController.home)
+
 // router.get('/ar', userController.mustBeLoggedIn, userController.openAR)
+
 router.get('/getAr', (req, res) => {
     res.render('ar')
 })
